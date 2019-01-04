ff45f38717ad9ddea0bd053ee61bd11660338a3c
Cory Gill <talktocory@gmail.com>
2019-01-04 11:42:52 -0500

    Added preliminary prototype of oidc-client login.  Message sent out to @jochen to see if he has a development identity server that can be used for testing.

---
+++ src/CampaignKit.WorldMap/wwwroot/js/worldmap-core.js
@@ -0,0 +1,68 @@
+﻿function log() {
+    document.getElementById('results').innerText = '';
+
+    Array.prototype.forEach.call(arguments, function (msg) {
+        if (msg instanceof Error) {
+            msg = "Error: " + msg.message;
+        }
+        else if (typeof msg !== 'string') {
+            msg = JSON.stringify(msg, null, 2);
+        }
+        document.getElementById('results').innerHTML += msg + '\r\n';
+    });
+}
+
+document.getElementById("login").addEventListener("click", login, false);
+document.getElementById("api").addEventListener("click", api, false);
+document.getElementById("logout").addEventListener("click", logout, false);
+
+const { protocol, hostname, port } = window.location;
+const rootUri = `${protocol}//${hostname}${port ? `:${port}` : ''}`;
+
+const settings = {
+    authority: 'https://campaign-identity.com',
+    client_id: 'worldmap.ui',
+    redirect_uri: `${rootUri}/oidc-callback`,
+    post_logout_redirect_uri: `${rootUri}/callback.html`,
+    automaticSilentRenew: true,
+    silent_redirect_uri: `${rootUri}/silent_renew.html`,
+    loadUserInfo: true,
+    response_type: 'token id_token',
+    scope: 'openid profile logger.ro',
+};
+
+var mgr = new Oidc.UserManager(settings);
+
+
+
+
+mgr.getUser().then(function (user) {
+    if (user) {
+        log("User logged in", user.profile);
+    }
+    else {
+        log("User not logged in");
+    }
+});
+
+function login() {
+    mgr.signinRedirect();
+}
+
+function api() {
+    mgr.getUser().then(function (user) {
+        var url = "http://localhost:5001/identity";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function () {
+            log(xhr.status, JSON.parse(xhr.responseText));
+        }
+        xhr.setRequestHeader("Authorization", "Bearer " + user.access_token);
+        xhr.send();
+    });
+}
+
+function logout() {
+    mgr.signoutRedirect();
+}
