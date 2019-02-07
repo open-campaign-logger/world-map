3f28ec4b5552325ccfb4cb7cd727d6b1ab062548
Cory Gill <talktocory@gmail.com>
2019-02-06 20:44:12 -0500

    Everything functioning again after fixing VisualStudio problems.  (default build targets went missing for some reason)

---
+++ tests/CampaignKit.WorldMap.Tests/ControllerTests/MapControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using CampaignKit.WorldMap.Tests.Infrastructure;
+using Xunit;
+
+
+namespace CampaignKit.WorldMap.Tests.ControllerTests
+{
+	public class MapControllerTests : IClassFixture<TestFixture<Startup, TestStartupAuth>>
+	{
+		private readonly TestFixture<Startup, TestStartupAuth> _testFixture;
+
+		public MapControllerTests(TestFixture<Startup, TestStartupAuth> testFixture)
+		{
+			this._testFixture = testFixture;
+		}
+
+		[Theory]
+		[InlineData("/Map/Create", "")]
+		[InlineData("/Map/Delete", "")]
+		[InlineData("/Map/Edit", "")]
+		[InlineData("/Map/Progress", "")]
+		public async Task TestAuthorizedGets(string page, string titleTestString)
+		{
+			var response = await _testFixture.Client.GetAsync(page);
+			response.EnsureSuccessStatusCode();
+
+		}
+
+	}
+}
